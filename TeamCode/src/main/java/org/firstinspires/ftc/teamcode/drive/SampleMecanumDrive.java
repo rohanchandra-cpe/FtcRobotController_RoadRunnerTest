6c8d3513de28025a5bb3f29d565a721a3cfc9025
tstauber <tom.stauber@gmail.com>
2023-06-27 19:01:06 -0400

    Changed Lateral Multiplier after running the strafe test in order to help the robot strafe the desired distance.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -61,8 +61,8 @@ public class SampleMecanumDrive extends MecanumDrive {
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
 
-    public static double LATERAL_MULTIPLIER = 1;
-
+    public static double LATERAL_MULTIPLIER = 1.35865947082;
+//Changed the lateral multiplier from 1 to 1.35865947082 from the strafe test
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
     public static double OMEGA_WEIGHT = 1;
